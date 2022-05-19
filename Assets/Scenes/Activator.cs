d4c779c558a90d1c3ee3988c0fd03aee799da38b
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-05-19 15:49:23 -0300

    Repasoooo instantiate

---
+++ Assets/Scenes/Activator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject myCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        abc();
+    }
+
+    void abc()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (myCube.activeInHierarchy)
+            {
+                myCube.SetActive(false);
+            }
+            else
+            {
+                myCube.SetActive(true);
+            }
+        }
+        myCube.SetActive(!myCube.activeInHierarchy);
+    }
+}
